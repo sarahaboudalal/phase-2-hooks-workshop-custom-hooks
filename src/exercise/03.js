79849305a14bb746d8ba1de7d971a177a7b56c1c
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-10-07 23:19:06 +0300

    exercise 3 done

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,7 +4,7 @@ import hasi from "../data/assets/hasi.png";
 export function useMouseCoordinates() {
   // ✅ get the setCoordinates function back too!
   // 👀 const [coordinates, setCoordinates] = useState(...)
-  const [coordinates] = useState({
+  const [coordinates, setCoordinates] = useState({
     clientX: 0,
     clientY: 0,
   });
@@ -15,7 +15,13 @@ export function useMouseCoordinates() {
      set state with the clientX and clientY coordinates from the event
      👀 function handler(event) {}
     */
-
+   function handleCoordinates(e){
+    setCoordinates({
+      clientX: e.clientX,
+      clientY: e.clientY
+    })
+   }
+   window.addEventListener("mousemove", handleCoordinates);
     /* 
      ✅ attach an event listener to the window for the mousemove event
      📃 https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event
@@ -23,6 +29,7 @@ export function useMouseCoordinates() {
     */
 
     return function cleanup() {
+      window.removeEventListener("mousemove", handleCoordinates)
       /* 
        ✅ make sure to clean up your event listeners when your hook is no longer in use!
        👀 window.removeEventListener("mousemove", handler)
